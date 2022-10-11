64afbe507668416f49af5d90dd6c9b78bf9b8673
serref jaguraga <reef163@gmail.com>
2022-10-11 18:58:35 -0400

    Added cart provider

---
+++ src/store/cart-context.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const CreateContext = React.createContext({
+  items: [],
+  totalAmount: 0,
+  addItem: (item) => {},
+  removeItem: (id) => {},
+});
+
+export default CreateContext;
